8ad15a8460338448ad9545d15179a2036bd7d1b2
Leakhana1223 <lyleakhana2@gmail.com>
2024-12-10 18:17:49 +0700

    last Update

--- PET Store/Pettype.Designer.cs
+++ PET Store/Pettype.Designer.cs
@@ -30,27 +30,27 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(paneltype));
             this.panelpet_type = new System.Windows.Forms.Panel();
-            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.no = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.petid = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.capitalize = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.type = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.active = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
             this.panelpet_type.SuspendLayout();
-            this.toolStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelpet_type
@@ -65,47 +65,103 @@
             this.panelpet_type.Controls.Add(this.label3);
             this.panelpet_type.Controls.Add(this.label1);
             this.panelpet_type.Controls.Add(this.toolStrip1);
-            this.panelpet_type.Location = new System.Drawing.Point(2, 3);
+            this.panelpet_type.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelpet_type.Location = new System.Drawing.Point(0, 0);
             this.panelpet_type.Name = "panelpet_type";
-            this.panelpet_type.Size = new System.Drawing.Size(1466, 945);
+            this.panelpet_type.Size = new System.Drawing.Size(1467, 991);
             this.panelpet_type.TabIndex = 0;
             // 
-            // toolStrip1
+            // dataGridView1
             // 
-            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
-            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripButton1,
-            this.toolStripButton2,
-            this.toolStripButton3});
-            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
-            this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(1466, 42);
-            this.toolStrip1.TabIndex = 1;
-            this.toolStrip1.Text = "toolStrip1";
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.no,
+            this.petid,
+            this.capitalize,
+            this.type,
+            this.active});
+            this.dataGridView1.Location = new System.Drawing.Point(5, 464);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 82;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(1461, 524);
+            this.dataGridView1.TabIndex = 48;
             // 
-            // toolStripButton1
+            // no
             // 
-            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
-            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton1.Name = "toolStripButton1";
-            this.toolStripButton1.Size = new System.Drawing.Size(119, 36);
-            this.toolStripButton1.Text = "Create";
+            this.no.HeaderText = "No";
+            this.no.MinimumWidth = 10;
+            this.no.Name = "no";
             // 
-            // toolStripButton2
+            // petid
             // 
-            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
-            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton2.Name = "toolStripButton2";
-            this.toolStripButton2.Size = new System.Drawing.Size(90, 36);
-            this.toolStripButton2.Text = "Edit";
+            this.petid.HeaderText = "Pet ID ";
+            this.petid.MinimumWidth = 10;
+            this.petid.Name = "petid";
             // 
-            // toolStripButton3
+            // capitalize
             // 
-            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
-            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton3.Name = "toolStripButton3";
-            this.toolStripButton3.Size = new System.Drawing.Size(120, 36);
-            this.toolStripButton3.Text = "Delete";
+            this.capitalize.HeaderText = "Capitalize Date";
+            this.capitalize.MinimumWidth = 10;
+            this.capitalize.Name = "capitalize";
+            // 
+            // type
+            // 
+            this.type.HeaderText = "Pet Type";
+            this.type.MinimumWidth = 10;
+            this.type.Name = "type";
+            // 
+            // active
+            // 
+            this.active.HeaderText = "Is_Active";
+            this.active.MinimumWidth = 10;
+            this.active.Name = "active";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.LightYellow;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox3.Location = new System.Drawing.Point(298, 380);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(118, 31);
+            this.textBox3.TabIndex = 47;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(81, 373);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(159, 37);
+            this.label4.TabIndex = 46;
+            this.label4.Text = "Is _Active :";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.LightYellow;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Location = new System.Drawing.Point(298, 288);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(501, 31);
+            this.textBox2.TabIndex = 45;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(81, 281);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(151, 37);
+            this.label2.TabIndex = 44;
+            this.label2.Text = "Pet Type :";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // textBox4
             // 
@@ -153,99 +209,42 @@
             this.label1.Text = "Pet ID :";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // textBox2
+            // toolStrip1
             // 
-            this.textBox2.BackColor = System.Drawing.Color.LightYellow;
-            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox2.Location = new System.Drawing.Point(298, 288);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(501, 31);
-            this.textBox2.TabIndex = 45;
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripButton3});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1467, 50);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
             // 
-            // label2
+            // toolStripButton1
             // 
-            this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.Black;
-            this.label2.Location = new System.Drawing.Point(81, 281);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(151, 37);
-            this.label2.TabIndex = 44;
-            this.label2.Text = "Pet Type :";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(119, 44);
+            this.toolStripButton1.Text = "Create";
             // 
-            // textBox3
+            // toolStripButton2
             // 
-            this.textBox3.BackColor = System.Drawing.Color.LightYellow;
-            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox3.Location = new System.Drawing.Point(298, 380);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(118, 31);
-            this.textBox3.TabIndex = 47;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(90, 36);
+            this.toolStripButton2.Text = "Edit";
             // 
-            // label4
+            // toolStripButton3
             // 
-            this.label4.AutoSize = true;
-            this.label4.BackColor = System.Drawing.Color.Transparent;
-            this.label4.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.Color.Black;
-            this.label4.Location = new System.Drawing.Point(81, 373);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(159, 37);
-            this.label4.TabIndex = 46;
-            this.label4.Text = "Is _Active :";
-            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.no,
-            this.petid,
-            this.capitalize,
-            this.type,
-            this.active});
-            this.dataGridView1.Location = new System.Drawing.Point(5, 464);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 82;
-            this.dataGridView1.RowTemplate.Height = 33;
-            this.dataGridView1.Size = new System.Drawing.Size(1460, 524);
-            this.dataGridView1.TabIndex = 48;
-            // 
-            // no
-            // 
-            this.no.HeaderText = "No";
-            this.no.MinimumWidth = 10;
-            this.no.Name = "no";
-            // 
-            // petid
-            // 
-            this.petid.HeaderText = "Pet ID ";
-            this.petid.MinimumWidth = 10;
-            this.petid.Name = "petid";
-            // 
-            // capitalize
-            // 
-            this.capitalize.HeaderText = "Capitalize Date";
-            this.capitalize.MinimumWidth = 10;
-            this.capitalize.Name = "capitalize";
-            // 
-            // type
-            // 
-            this.type.HeaderText = "Pet Type";
-            this.type.MinimumWidth = 10;
-            this.type.Name = "type";
-            // 
-            // active
-            // 
-            this.active.HeaderText = "Is_Active";
-            this.active.MinimumWidth = 10;
-            this.active.Name = "active";
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(120, 36);
+            this.toolStripButton3.Text = "Delete";
             // 
             // paneltype
             // 
@@ -256,11 +255,12 @@
             this.Name = "paneltype";
             this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
             this.Text = "Pettype";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.panelpet_type.ResumeLayout(false);
             this.panelpet_type.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
